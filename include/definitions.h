3a74c59c7bf12c883811d06da612352d0755c1b1
BradyCates <bcates5@outlook.com>
2026-08-07 13:44:36 -0600

    Fixed robot moving backwards

--- include/definitions.h
+++ include/definitions.h
@@ -8,8 +8,8 @@ float driving_speed = 127;
 int autonomous_mode = 1;
 int driving_mode = 1;
 pros::Controller master(pros::E_CONTROLLER_MASTER);
-pros::MotorGroup DrivetrainL({-19, -20}); 
-pros::MotorGroup DrivetrainR({11, 12});    
+pros::MotorGroup DrivetrainL({19, 20}); 
+pros::MotorGroup DrivetrainR({-11, -12});    
 pros::MotorGroup Lift({7, -8, -9, 10});
 pros::Motor ColorSwitcher
 (5);
